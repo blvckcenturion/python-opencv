654a6729412168b1f0d55e95b40d8af24b0140a9
blvckcenturion <blvckcenturion@gmail.com>
2022-06-03 15:10:51 -0400

    Finished 3 examples

---
+++ arithmetic/script.py
@@ -0,0 +1,64 @@
+import numpy as np
+import cv2
+
+# load the image and show it 
+image = cv2.imread("./images/grand_canyon.png")
+cv2.imshow("Original", image)
+# The images are NumPy arrays stored as 8 bit unsigned integers
+# Los valores de nuestros pixeles estaran en el rango [0, 255]  QUE OPINA ????????? ES ASI?????
+
+# The value of the pixel is not in range of 255 as a each pixel
+# is represented as a tuple containing 3 values (b,g,r) which they go from 0 to 255
+
+# Using functions like cv2.add y cv2.subtract, the values are going to be added or subtracted to the range we are working with.
+# QUE SUCEDE SI LOS VALORES ADICIONADOS O RESTADOS ESTAN FUERA DEL RANGO [0, 255]. ?????????
+# If the result of the add is 255+ it will set it as 255
+# If the result of the subtract is 0- it will set it as 0
+
+# COMENTA QUE HACEN LAS SIGUIENTES 2 LINEAS DE CODIGO
+# First line we are adding 200 + 100 using openCV's add function and then printing the result
+# Which in this case returns 255 as it is the value it defaults to
+print("max of 255: {}".format(str(cv2.add(np.uint8([200]), np.uint8([100])))))
+
+# Second line we are subtracting 50 - 100 using openCV's subtract function and then printing the result
+# Which in this case returns 0 as it is the value it defaults to
+print("min of 0: {}".format(str(cv2.subtract(np.uint8([50]), np.uint8([100])))))
+
+# Si se utiliza operaciones aritmeticas en estas matrices que sucede con los valores????
+#   QUE HACE EL WRAP AROUND ???? QUE SUCEDE CON LAS DISTANCIAS??
+#  ES IMPORTANTE TENER EN CUENTA TODOS ESTOS FACTORES CUANDO TRABAJAMOS CON IMAGENES?
+print("wrap around: {}".format(str(np.uint8([200]) + np.uint8([100]))))
+print("wrap around: {}".format(str(np.uint8([50]) - np.uint8([100]))))
+
+# aumentemos la intensidad de todos los pixeles en nuestra imagen por 100?
+# ESTO SE LOGRA MEDIANTE LA CONSTRUCCION DE UNA MATRIZ (CON SOLO VALORES 1) Y MULTIPLICAR CADA VALOR POR 100???
+# DESPUES SOLO AGREGAMOS LA IMAGEN PARA OBTENER UN TONO MAS BRILLANTE ???????? ES VERDAD????
+# COMO OBTENGO UNA IMAGEN MAS BRILLANTE??????
+
+# We are building a NumPy array using the image shape as the argument for the number of rows and columns our
+# new numpy array should contain
+M = np.ones(image.shape, dtype = "uint8") * 100
+
+print(image[0,0])
+# Then we use openCV to add that Numpy array to our original image, and what this will do
+# is that it will add 100 to each of the pixels in the original image
+# Meaning if pixel at position 0,0 had as values (100,50,70) after the cv2.add 
+# That same pixel will hold the values (200,150,170)
+
+added = cv2.add(image, M)
+print(added[0,0])
+cv2.imshow("VALOR ADICIONADO", added)
+# SUCEDE LO MISMO SI RESTAMOS 50?????? OBTENEMOS UNA IMAGEN MAS OSCURA
+# COMO FUNCIONA ??????
+
+# The subtraction works just as the addition but instead of adding the value to each position,
+# we are going to subtract that value from each pixel's rgb values
+# Meaning if pixel at position 0,0 had as values (100,100,70) after the cv2.subtract 
+# That same pixel will hold the values (50,50,20)
+
+M = np.ones(image.shape, dtype = "uint8") * 50
+print(image[0,0])
+subtracted = cv2.subtract(image, M)
+print(subtract[0,0])
+cv2.imshow("VALOR RESTADO", subtracted)
+cv2.waitKey(0)
--- basic_image/script.py
+++ basic_image/script.py
@@ -1,6 +1,6 @@
 import cv2
 
-image = cv2.imread("jurasic.jpg")
+image = cv2.imread("./images/jurasic.jpg")
 # Loading the image, then destructuring the height and width of the image from the image's shape attribute.
 # :2 takes the last two elements of the tuple. 
 (h, w) = image.shape[:2]
@@ -9,30 +9,47 @@ cv2.imshow("Original", image)
 # The left top pixel is at (0, 0) of the image returned by the cv2.imread() function.
 # In the next line we destructure the red, green and blue values of the pixel at (0, 0) and print them.
 (b, g, r) = image[0, 0]
-# que hace la siguiente linea de codigo (9)? ........
 
+# que hace la siguiente linea de codigo (9)? ........
+# Printing the values of red, green and blue taken from the left top pixel(0,0)
 print("Pixel at (0, 0) - Red: {r}, Green: {g}, Blue: {b}".format(r=r, g=g, b=b))
 # ahora, cambiemos el valor del pixel en (0, 0) y hagamos que sea rojo  es verdad ? analicen el codigo........
+# In this case by assigning (255,0,0) to the pixel will make it blue due to the order used in openCV
 image[0, 0] = (255, 0, 0)
 (b, g, r) = image[0, 0]
 print("Pixel at (0, 0) - Red: {r}, Green: {g}, Blue: {b}".format(r=r, g=g, b=b))
 # calcular el centro de la imagen, que es simplemente el ancho y la altura
 # dividiendo por dos ?......
+
 (cX, cY) = (w // 2, h // 2)
+# We can prove that by performing the operation above we get as a result the center of each axis
+print(f"width: {w} | center X: {cX}")
+print(f"height: {h} | center X: {cY}")
 # ya que estamos usando matrices NumPy ??????, podemos aplicar cortes y obtener partes
 # de la imagen a partir de la esquina superior izquierda, es cierto ????????????
+
+# The image is a NumPy array
+# And the stated argument is true
+# Becase we are getting the values from position 0, until position cX & cY.
 tl = image[0:cY, 0:cX]
 cv2.imshow("esquina superior-izquierda", tl)
+
 # de forma similar, agarremos la parte superior derecha, la inferior derecha y la inferior izquierda
 # cada parte la mostramos en pantalla
+# Upper Right Corner 
 tr = image[0:cY, cX:w]
+# Lower Right Corner 
 br = image[cY:h, cX:w]
+# Upper Left Corner
 bl = image[cY:h, 0:cX]
 cv2.imshow("esquina superior-derecha", tr)
 cv2.imshow("esquina inferior-derecha", br)
 cv2.imshow("esquina inferior izquierda", bl)
+
 # ahora vamos a hacer que la esquina superior izquierda de la imagen original este verde,  es verdad ?.........
-image[0:cY, 0:cX] = (255, 0,0)
-# Mostramos nuestra imagen actualizada
+# In the next line we change the values of the upper left corner to blue, so the statement made in the upper line is false.
+image[0:cY, 0:cX] = (509, 0,0)
+
+# Showing the updated image
 cv2.imshow("actualizada", image)
 cv2.waitKey(0)
